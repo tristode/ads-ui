54edc60fbdf9249232ae2ae8d2e189805e5d5314
rizerphe <44440399+rizerphe@users.noreply.github.com>
2024-04-01 19:58:47 +0300

    Fix supabase and add authentication

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing env variables for Supabase");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
