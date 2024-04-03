bbf1381e9ebffada3097a1a4ae7bdc3c40a02cc4
rizerphe <44440399+rizerphe@users.noreply.github.com>
2024-04-03 12:03:58 +0300

    Add post exclusivity management

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -10,7 +10,13 @@ import { FaHeart } from "react-icons/fa";
 import { MdShare } from "react-icons/md";
 import { useReplying } from "./single-reply-box-provider";
 
-export default function PostCard({ post }: { post: Post }) {
+export default function PostCard({
+  post,
+  exclusive,
+}: {
+  post: Post;
+  exclusive?: boolean;
+}) {
   const liked = post.reactedByLoggedInUser?.includes("like");
   const replying = useReplying();
 
@@ -42,14 +48,14 @@ export default function PostCard({ post }: { post: Post }) {
       </div>
       {/** Reactions - the like button mainly */}
       <div className="p-4 flex justify-around">
-        <div className="flex flex-col items-center cursor-pointer">
+        <div className="flex flex-col items-center cursor-pointer w-full">
           <FaHeart className={liked ? "text-red-500" : "text-gray-500"} />
           <span className="font-black text-xs text-gray-500 dark:text-gray-300">
             {post.reactions?.like || 0}
           </span>
         </div>
         <div
-          className="flex flex-col items-center cursor-pointer"
+          className="flex flex-col items-center cursor-pointer w-full"
           onClick={() => replying.setParentId(post.id)}
         >
           <BsChatLeftFill className="text-gray-500" />
@@ -57,16 +63,23 @@ export default function PostCard({ post }: { post: Post }) {
             {post.replies.length}
           </span>
         </div>
-        <div className="flex flex-col items-center cursor-pointer">
+        <div className="flex flex-col items-center cursor-pointer w-full">
           <MdShare className="text-gray-500" />
           <span className="font-black text-xs text-gray-500 dark:text-gray-300">
             Share
           </span>
         </div>
       </div>
-      {(post.id === replying.parentId || !replying.parentId) && (
-        <AddComment parentId={post.id} />
-      )}
+      <AddComment
+        parentId={post.id}
+        className={
+          post.id === replying.parentId || (exclusive && !replying.parentId)
+            ? ""
+            : "hidden sm:block"
+        }
+        parentAuthorHandle={exclusive ? undefined : post.author.handle}
+        onCancel={() => replying.setParentId(undefined)}
+      />
       {post.replies?.map((reply) => (
         <Comment key={reply.id} comment={reply} />
       ))}
