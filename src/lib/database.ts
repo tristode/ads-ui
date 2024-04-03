bf7cd6f9e72ebbf873f281565fec08ca866dba2d
rizerphe <44440399+rizerphe@users.noreply.github.com>
2024-04-03 12:10:24 +0300

    Add useUser

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,4 +1,5 @@
 import { AddCommentArgs, Post } from "@/types";
+import { User } from "@supabase/supabase-js";
 
 // To use "useAuthSession" from "./auth.ts"
 
@@ -6,6 +7,7 @@ export const usePostPreview = (postId: string): Post | null => null;
 // Loads all comments
 export const usePost = (postId: string): Post | null => null;
 export const useComment = (commentId: string): Comment | null => null;
+export const useUser = (userId: string): User | null => null;
 
 export const setLike = (postId: string): void => undefined;
 export const reply = (parentId: string, args: AddCommentArgs): void =>
