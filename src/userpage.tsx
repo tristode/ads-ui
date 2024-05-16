a3126e3cf832d144d6d245a8b1879d8d1e830b14
Romko-studik <zaletskyi.pn@ucu.edu.ua>
2024-05-16 17:40:39 +0300

    Add actual userpage

--- src/userpage.tsx
+++ src/userpage.tsx
@@ -1,3 +1,17 @@
-export default function Null() {
-  return <></>;
+import { useUser } from "./lib/database";
+import {useParams} from "react-router-dom";
+
+import{
+
+} from "@/components/ui/form";
+
+export default function ProfilePage(){
+    const { userId } = useParams();
+    const user = useUser(userId ?? "");
+    return(
+        <div>
+            
+            <span>{user?.handle}</span>
+        </div>
+    )
 }
