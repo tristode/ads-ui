20e762f2e2078531d67b7248e67313cfb749f600
Romko-studik <zaletskyi.pn@ucu.edu.ua>
2024-05-16 18:23:30 +0300

    Add displaying user info and avata

--- src/userpage.tsx
+++ src/userpage.tsx
@@ -1,17 +1,36 @@
 import { useUser } from "./lib/database";
 import {useParams} from "react-router-dom";
+import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
+import Checkmark from "./components/ui/checkmark";
 
-import{
-
-} from "@/components/ui/form";
 
 export default function ProfilePage(){
     const { userId } = useParams();
     const user = useUser(userId ?? "");
     return(
-        <div>
-            
-            <span>{user?.handle}</span>
+        <div className="flex flex-col items-center justify-center pt-16">
+        <div className="flex flex-col items-start justify-center w-full md:w-1/2">
+            <Avatar className="w-12 h-12">
+              <AvatarImage alt={user?.name} src={user?.avatar} />
+              <AvatarFallback>{user?.name[0]}</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col gap-0">
+              <div className="flex items-center space-x-2">
+                <span className="font-bold">{user?.name}</span>
+                <div className="flex flex-row gap-0">
+                  {user?.checkmarks?.map((type) => (
+                    <Checkmark key={type} type={type} />
+                  ))}
+                </div>
+              </div>
+              <span className="font-black text-xs text-gray-500">
+                @{user?.handle}  
+              </span>
+              <span className="font-black text-xs text-gray-500">
+                {user?.bio}
+              </span>
         </div>
+        </div>
+    </div>
     )
 }
