e136f0e62dfce5d22a85ad9e6345d92860444382
rhusiev <h.radomyr@proton.me>
2024-05-17 10:47:19 +0300

    Add margin to upload img button

--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -63,7 +63,7 @@ export default function ImageUploader({
                         </div>
                     ))}
             </div>
-            <div className="flex flex-wrap items-center gap-2">
+            <div className="flex flex-wrap items-center gap-2 mt-2">
                 {notUploadedImages.map((file, index) => (
                     <div key={file.name} className="relative">
                         <img
